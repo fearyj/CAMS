152049ac421c77725e89f688bbb3cf2c5215c4af
rxxchel <107982625+rxxchel@users.noreply.github.com>
2023-10-29 14:53:06 +0800

    Update Report.java

--- CampSystem/Report.java
+++ CampSystem/Report.java
@@ -9,43 +9,43 @@ public class Report {
         try (FileWriter fw = new FileWriter(file, false);
                 BufferedWriter bw = new BufferedWriter(fw)) 
         {
-        	
-        	//camp->name is camp name or participant name?
-        	bw.write(camp.getName());
-            bw.newLine();
+             //camp->name is camp name or participant name?
+             bw.write(camp.getName());
+             bw.newLine();
                
-            bw.write("Start Date: "+String.valueOf(camp.getStartDate()));
-            bw.newLine();
+             bw.write("Start Date: "+String.valueOf(camp.getStartDate()));
+             bw.newLine();
             
-            bw.write("End Date: "+String.valueOf(camp.getEndDate()));
-            bw.newLine();
+             bw.write("End Date: "+String.valueOf(camp.getEndDate()));
+             bw.newLine();
             
-            bw.write("Registration Closing Date: "+String.valueOf(camp.getRegisterBy()));
-            bw.newLine();
+             bw.write("Registration Closing Date: "+String.valueOf(camp.getRegisterBy()));
+             bw.newLine();
             
-            bw.write("Group: "+camp.getGroup());
-            bw.newLine();
+             bw.write("Group: "+camp.getGroup());
+             bw.newLine();
             
-            bw.write("Location: "+camp.getLocation());
-            bw.newLine();
+             bw.write("Location: "+camp.getLocation());
+             bw.newLine();
             
-            bw.write("Total Slots: "+camp.getTotalSlots());
-            bw.newLine();
+             bw.write("Total Slots: "+camp.getTotalSlots());
+             bw.newLine();
             
-            bw.write("Camp Committee Slots: "+camp.getCommitteeSlots());
-            bw.newLine();
+             bw.write("Camp Committee Slots: "+camp.getCommitteeSlots());
+             bw.newLine();
             
-            bw.write("Description: "+camp.getDescription());
-            bw.newLine();
+             bw.write("Description: "+camp.getDescription());
+             bw.newLine();
             
-            bw.write("Staff in Charge: "+camp.getStaffInCharge());
-            bw.newLine();
+             bw.write("Staff in Charge: "+camp.getStaffInCharge());
+             bw.newLine();
             
-            bw.write("Attendee List: "+camp.getAttendeeList());
-            bw.newLine();
+             bw.write("Attendee List: "+camp.getAttendeeList());
+             bw.newLine();
             
-            bw.write("Camp Committee List: "+camp.getCommitteeList());
-            bw.newLine();
+             bw.write("Camp Committee List: "+camp.getCommitteeList());
+             bw.newLine();
+             
             /*if (camp.enrolledAttendee(student)) 
             {
             	bw.write(camp.getName() + ": Attendee");
